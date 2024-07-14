afafeab7a706411539abeb6d0af3758e61f97bfb
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-07-15 02:04:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function createTable() {
     //Write your code here
+	let num= parseInt(prompt("input number of rows"));
+
+	let table=document.createElement("table");
+	table.style.border="1px solid black";
+	
+	for(let i=0; i<num; i++){
+		let row=document.createElement("tr");
+		for(let j=0; j<3; j++){
+			let col=document.createElement("td");
+			col.style.border="1px solid black";
+			col.innerHTML=`Row-${i} Column-${j}`
+			row.append(col);
+		}
+		table.append(row);
+	}
+
+	document.body.prepend(table);
   
 }
